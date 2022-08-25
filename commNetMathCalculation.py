3142f4c889f85f5feeee97c1865f2860d85ae88a
xhc12345 <kevin20020613@gmail.com>
2022-08-25 15:57:40 -0500

    completed math for orbit calculation

--- commNetMathCalculation.py
+++ commNetMathCalculation.py
@@ -1,5 +1,25 @@
-G = 6.67408e-11 # Newton's gravitational constant
+from math import pi, sqrt
+
+from commNetMathOutput import commNetMathOutput
+
+
+GRAVITATIONAL_CONSTANT = 6.67408e-11 # Newton's gravitational constant
 
 class commNetMathCalculation:
-    def get(objMassKG):
-        stdGravParamM = G * objMassKG
+    def computeResult(input):
+        stdGravParamM = GRAVITATIONAL_CONSTANT * input.bodyMassKilogram
+        stdGravParamKM = stdGravParamM / (1000 ** 3)
+
+        finalOrbitRadiusKM = (input.targetOrbitHeightMeter + input.bodyRadiusMeter) / 1000
+        timeFinal = 2 * pi * sqrt(finalOrbitRadiusKM**3 / stdGravParamKM)
+        timeInitial = (input.numberOfSatellites-1)/input.numberOfSatellites * timeFinal
+
+        a_i = ((stdGravParamKM * timeInitial**2) / (4 * pi * pi)) ** (1./3.)
+        initialOrbitPerigee = (2*a_i - finalOrbitRadiusKM) * 1000
+        initialOrbitPerigeeHeightM = initialOrbitPerigee - input.bodyRadiusMeter
+
+        velocityFinal = sqrt(stdGravParamKM / finalOrbitRadiusKM)
+        velocityInitial = sqrt((2/finalOrbitRadiusKM - 1/a_i) * stdGravParamKM)
+        deltaVelocityMeterPerSecond = (velocityFinal - velocityInitial) * 1000
+
+        return commNetMathOutput(initialOrbitPerigeeHeightM, deltaVelocityMeterPerSecond, timeInitial, timeFinal)
